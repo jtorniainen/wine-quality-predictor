02639ae3d5027846fd3cf0c83de6b29259718fe2
arun <arunisnowhere@gmail.com>
2018-08-15 14:59:09 +0300

    add recursive feature elimination for regression

--- wine_linreg_vs_automl.py
+++ wine_linreg_vs_automl.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     # Train-Test split
     df_train, df_test = train_test_split(df, test_size=.20, random_state=1)
 
-    X_train = df_train.drop('quality', axis=1)
+    X_train = df_train.drop('quality', axis=1) 
     y_train = df_train['quality']
 
     X_test = df_test.drop('quality', axis=1)
@@ -49,15 +49,44 @@ if __name__ == '__main__':
 #    model = SelectFromModel(clf, prefit=True)
 #    X_train_fsel = model.transform(X_train)
 #    X_test_fsel = model.transform(X_test)
+#    
+#    from sklearn.svm import SVC    
+#    from sklearn.feature_selection import RFE
+#    svc = SVC(kernel="linear", C=1)
+#    rfe = RFE(estimator=svc, n_features_to_select=4, step=1)
+#    rfe.fit(X_train, y_train)
     
+    from sklearn.datasets import make_friedman1
+    from sklearn.feature_selection import RFE
+    from sklearn.svm import SVR
     
+    for nfeatures in range(1,12):
     
-    reg = linear_model.LinearRegression()
-    reg.fit(X_train_fsel, y_train)
-    linear_model.LinearRegression(copy_X=True, fit_intercept=True, n_jobs=1, normalize=False)
-   
-    reg_fsel_y_pred = reg.predict(X_test_fsel)
-    reg_fsel_y_pred_int = np.round(reg_fsel_y_pred)
+        estimator = SVR(kernel="linear")
+        selector = RFE(estimator, nfeatures, step=1)
+        selector = selector.fit(X_train, y_train)
+        print(selector.support_)
+        
+        features_selected = []
+        for i in range(0,selector.support_.size):
+            if selector.support_[i]==True:
+                features_selected.append(i)
+            
+        X_train_fsel = X_train[X_train.columns[features_selected]]
+        X_test_fsel = X_test[X_test.columns[features_selected]]    
+        
+        reg = linear_model.LinearRegression()
+        reg.fit(X_train_fsel, y_train)
+        linear_model.LinearRegression(copy_X=True, fit_intercept=True, n_jobs=1, normalize=False)
+       
+        reg_fsel_y_pred = reg.predict(X_test_fsel)
+        reg_fsel_y_pred_int = np.round(reg_fsel_y_pred)
+ 
+    #    # Linear Regression with feature selection
+        print("No of features, Mean squared error, Accuracy", nfeatures, round(mean_squared_error(y_test, reg_fsel_y_pred)*100), round(metrics.accuracy_score(y_test, reg_fsel_y_pred_int)*100))
+#        print("Mean squared error for linear regression with feature selection: ", round(mean_squared_error(y_test, reg_fsel_y_pred)*100))
+#        print("Accuracy for linear regression  with feature selection:", round(metrics.accuracy_score(y_test, reg_fsel_y_pred_int)*100))
+        
       
     
     # ----- 1b. Random Forest -----
@@ -77,15 +106,15 @@ if __name__ == '__main__':
     # Initialize auto learning object
     automl = autosklearn.regression.AutoSklearnRegressor(time_left_for_this_task=120,
                                                          per_run_time_limit=30,
-                                                         tmp_folder='/tmp/kekkonen1412412412413412124',
-                                                         output_folder='/tmp/wine-out1241243124kj214124412124kjj')
+                                                         tmp_folder='/tmp/kekkonen2412gjhgj412413412124',
+                                                         output_folder='/tmp/wine-out12412431ghjghj24kj21412444kjj')
 
     # Fit data
     automl.fit(X_train, y_train, dataset_name='wine-quality', feat_type=feature_types)
 
     # Get prediction for test set
     automl_y_pred = automl.predict(X_test)
-    automl_y_pred_int = np.round(y_pred)
+    automl_y_pred_int = np.round(automl_y_pred)
     r2 = r2_score(y_test, automl_y_pred_int)
        
     # Visualize results
@@ -101,14 +130,12 @@ if __name__ == '__main__':
     
     # Linear Regression
     print("Mean squared error for linear regression: ", round(mean_squared_error(y_test, reg_y_pred)*100))
-    print("Accuracy for linear regression:", round(metrics.accuracy_score(y_test, reg_y_pred_int))*100)   
-    # Linear Regression with feature selection
-    print("Mean squared error for linear regression with feature selection: ", round(mean_squared_error(y_test, reg_fsel_y_pred)*100))
-    print("Accuracy for linear regression  with feature selection:", round(metrics.accuracy_score(y_test, reg_fsel_y_pred_int)*100))
+    print("Accuracy for linear regression:", round(metrics.accuracy_score(y_test, reg_y_pred_int)*100))
+    
     # Random Forest
-    print("Mean squared error for random forest: ", mean_squared_error(y_test, rfc_y_pred_int))
-    print("Accuracy for random forest:", metrics.accuracy_score(y_test, rfc_y_pred_int))    
+    print("Mean squared error for random forest: ", round(mean_squared_error(y_test, rfc_y_pred_int)*100))
+    print("Accuracy for random forest:", round(metrics.accuracy_score(y_test, rfc_y_pred_int)*100))
     # Auto ML
-    print("Mean squared error for AutoML: ", mean_squared_error(y_test, automl_y_pred_int))
-    print("Accuracy for AutoML:", metrics.accuracy_score(y_test, automl_y_pred_int))
+    print("Mean squared error for AutoML: ", round(mean_squared_error(y_test, automl_y_pred_int)*100))
+    print("Accuracy for AutoML:", round(metrics.accuracy_score(y_test, automl_y_pred_int)*100))
     
