1ea0cfa85c206cadb9ecab37a77f793ce89b503f
arun <arunisnowhere@gmail.com>
2018-08-15 15:38:49 +0300

    clean code

--- wine_linreg_vs_automl.py
+++ wine_linreg_vs_automl.py
@@ -42,6 +42,7 @@ if __name__ == '__main__':
     
     # With feature selection
     
+    # using extra trees classified
 #    from sklearn.ensemble import ExtraTreesClassifier
 #    from sklearn.feature_selection import SelectFromModel
 #    clf = ExtraTreesClassifier()
@@ -56,39 +57,49 @@ if __name__ == '__main__':
 #    rfe = RFE(estimator=svc, n_features_to_select=4, step=1)
 #    rfe.fit(X_train, y_train)
     
-    from sklearn.datasets import make_friedman1
+    # using recursive feature elimination
     from sklearn.feature_selection import RFE
     from sklearn.svm import SVR
-    
-    for nfeatures in range(1,12):
-    
-        estimator = SVR(kernel="linear")
-        selector = RFE(estimator, nfeatures, step=1)
-        selector = selector.fit(X_train, y_train)
-        print(selector.support_)
         
-        features_selected = []
-        for i in range(0,selector.support_.size):
-            if selector.support_[i]==True:
-                features_selected.append(i)
-            
-        X_train_fsel = X_train[X_train.columns[features_selected]]
-        X_test_fsel = X_test[X_test.columns[features_selected]]    
+    # code to find out optimal number of features
+#    for nfeatures in range(1,12):    
+#        estimator = SVR(kernel="linear")
+#        selector = RFE(estimator, nfeatures, step=1)
+#        selector = selector.fit(X_train, y_train)
+#        print(selector.support_)
         
-        reg = linear_model.LinearRegression()
-        reg.fit(X_train_fsel, y_train)
-        linear_model.LinearRegression(copy_X=True, fit_intercept=True, n_jobs=1, normalize=False)
-       
-        reg_fsel_y_pred = reg.predict(X_test_fsel)
-        reg_fsel_y_pred_int = np.round(reg_fsel_y_pred)
+#        features_selected = []
+#        for i in range(0,selector.support_.size):
+#            if selector.support_[i]==True:
+#                features_selected.append(i)
+#            
+#        X_train_fsel = X_train[X_train.columns[features_selected]]
+#        X_test_fsel = X_test[X_test.columns[features_selected]]    
+#        
+#        reg = linear_model.LinearRegression()
+#        reg.fit(X_train_fsel, y_train)
+#        linear_model.LinearRegression(copy_X=True, fit_intercept=True, n_jobs=1, normalize=False)
+#       
+#        reg_fsel_y_pred = reg.predict(X_test_fsel)
+#        reg_fsel_y_pred_int = np.round(reg_fsel_y_pred)
+#        print("No of features, Mean squared error, Accuracy", nfeatures, round(mean_squared_error(y_test, reg_fsel_y_pred)*100), round(metrics.accuracy_score(y_test, reg_fsel_y_pred_int)*100))
  
-    #    # Linear Regression with feature selection
-        print("No of features, Mean squared error, Accuracy", nfeatures, round(mean_squared_error(y_test, reg_fsel_y_pred)*100), round(metrics.accuracy_score(y_test, reg_fsel_y_pred_int)*100))
-#        print("Mean squared error for linear regression with feature selection: ", round(mean_squared_error(y_test, reg_fsel_y_pred)*100))
-#        print("Accuracy for linear regression  with feature selection:", round(metrics.accuracy_score(y_test, reg_fsel_y_pred_int)*100))
-        
-      
+    # optimal number of features was found to be 6.
+    estimator = SVR(kernel="linear")
+    selector = RFE(estimator, 6, step=1)
+    selector = selector.fit(X_train, y_train)
+    print(selector.support_)
     
+    X_train_fsel = X_train[X_train.columns[features_selected]]
+    X_test_fsel = X_test[X_test.columns[features_selected]]    
+    
+    reg = linear_model.LinearRegression()
+    reg.fit(X_train_fsel, y_train)
+    linear_model.LinearRegression(copy_X=True, fit_intercept=True, n_jobs=1, normalize=False)
+   
+    reg_fsel_y_pred = reg.predict(X_test_fsel)
+    reg_fsel_y_pred_int = np.round(reg_fsel_y_pred)
+          
     # ----- 1b. Random Forest -----
     
     from sklearn.ensemble import RandomForestClassifier    
@@ -106,8 +117,8 @@ if __name__ == '__main__':
     # Initialize auto learning object
     automl = autosklearn.regression.AutoSklearnRegressor(time_left_for_this_task=120,
                                                          per_run_time_limit=30,
-                                                         tmp_folder='/tmp/kekkonen2412gjhgj412413412124',
-                                                         output_folder='/tmp/wine-out12412431ghjghj24kj21412444kjj')
+                                                         tmp_folder='/tmp/kekkonen2412gj13412124',
+                                                         output_folder='/tmp/wine-out12412431g1412444kjj')
 
     # Fit data
     automl.fit(X_train, y_train, dataset_name='wine-quality', feat_type=feature_types)
@@ -131,7 +142,9 @@ if __name__ == '__main__':
     # Linear Regression
     print("Mean squared error for linear regression: ", round(mean_squared_error(y_test, reg_y_pred)*100))
     print("Accuracy for linear regression:", round(metrics.accuracy_score(y_test, reg_y_pred_int)*100))
-    
+    # Linear Regression with feature selection        
+    print("Mean squared error for linear regression with feature selection: ", round(mean_squared_error(y_test, reg_fsel_y_pred)*100))
+    print("Accuracy for linear regression  with feature selection:", round(metrics.accuracy_score(y_test, reg_fsel_y_pred_int)*100))    
     # Random Forest
     print("Mean squared error for random forest: ", round(mean_squared_error(y_test, rfc_y_pred_int)*100))
     print("Accuracy for random forest:", round(metrics.accuracy_score(y_test, rfc_y_pred_int)*100))
