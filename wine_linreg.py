dccdbdf2c85ba31f00998a3a2235f7b4506bbac8
arun <arunisnowhere@gmail.com>
2018-08-16 13:29:17 +0300

    create new file for lin_reg_feature_sel

--- wine_linreg.py
+++ wine_linreg.py
@@ -6,7 +6,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn import linear_model
 from sklearn import metrics
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.metrics import mean_squared_error
 
 if __name__ == '__main__':
     # Load data from CSV
@@ -22,11 +22,6 @@ if __name__ == '__main__':
     X_test = df_test.drop('quality', axis=1)
     y_test = df_test['quality']
 
-    # ----- 1. Manual -----
-    # ----- 1a. Linear regression -----
-    
-    # Without feature selection
-    
     reg = linear_model.LinearRegression()
     reg.fit(X_train, y_train)
     linear_model.LinearRegression(copy_X=True, fit_intercept=True, n_jobs=1, normalize=False)
@@ -40,79 +35,13 @@ if __name__ == '__main__':
     sns.distplot(reg_y_pred_int, kde=False, ax=ax)
     ax.set_title('Linear Regression')
     
-    # With feature selection
-    
-    # using extra trees classified
-#    from sklearn.ensemble import ExtraTreesClassifier
-#    from sklearn.feature_selection import SelectFromModel
-#    clf = ExtraTreesClassifier()
-#    clf = clf.fit(X_train, y_train)    
-#    model = SelectFromModel(clf, prefit=True)
-#    X_train_fsel = model.transform(X_train)
-#    X_test_fsel = model.transform(X_test)
-#    
-#    from sklearn.svm import SVC    
-#    from sklearn.feature_selection import RFE
-#    svc = SVC(kernel="linear", C=1)
-#    rfe = RFE(estimator=svc, n_features_to_select=4, step=1)
-#    rfe.fit(X_train, y_train)
-    
-    # using recursive feature elimination
-    from sklearn.feature_selection import RFE
-    from sklearn.svm import SVR
-        
-    # code to find out optimal number of features
-#    for nfeatures in range(1,12):    
-#        estimator = SVR(kernel="linear")
-#        selector = RFE(estimator, nfeatures, step=1)
-#        selector = selector.fit(X_train, y_train)
-#        print(selector.support_)
-        
-#        features_selected = []
-#        for i in range(0,selector.support_.size):
-#            if selector.support_[i]==True:
-#                features_selected.append(i)
-#            
-#        X_train_fsel = X_train[X_train.columns[features_selected]]
-#        X_test_fsel = X_test[X_test.columns[features_selected]]    
-#        
-#        reg = linear_model.LinearRegression()
-#        reg.fit(X_train_fsel, y_train)
-#        linear_model.LinearRegression(copy_X=True, fit_intercept=True, n_jobs=1, normalize=False)
-#       
-#        reg_fsel_y_pred = reg.predict(X_test_fsel)
-#        reg_fsel_y_pred_int = np.round(reg_fsel_y_pred)
-#        print("No of features, Mean squared error, Accuracy", nfeatures, round(mean_squared_error(y_test, reg_fsel_y_pred)*100), round(metrics.accuracy_score(y_test, reg_fsel_y_pred_int)*100))
- 
-    # optimal number of features was found to be 6.
-    estimator = SVR(kernel="linear")
-    selector = RFE(estimator, 6, step=1)
-    selector = selector.fit(X_train, y_train)
-    print(selector.support_)
-    
-    # select the features, i.e., column numbers
-    features_selected = []
-    for i in range(0,selector.support_.size):
-        if selector.support_[i]==True:
-            features_selected.append(i)
-    
-    X_train_fsel = X_train[X_train.columns[features_selected]]
-    X_test_fsel = X_test[X_test.columns[features_selected]]    
-    
-    reg = linear_model.LinearRegression()
-    reg.fit(X_train_fsel, y_train)
-    linear_model.LinearRegression(copy_X=True, fit_intercept=True, n_jobs=1, normalize=False)
-   
-    reg_fsel_y_pred = reg.predict(X_test_fsel)
-    reg_fsel_y_pred_int = np.round(reg_fsel_y_pred)          
     
     
     # Linear Regression
     print("Mean squared error for linear regression: ", round(mean_squared_error(y_test, reg_y_pred)*100))
     print("Accuracy for linear regression:", round(metrics.accuracy_score(y_test, reg_y_pred_int)*100))
-    # Linear Regression with feature selection        
-    print("Mean squared error for linear regression with feature selection: ", round(mean_squared_error(y_test, reg_fsel_y_pred)*100))
-    print("Accuracy for linear regression  with feature selection:", round(metrics.accuracy_score(y_test, reg_fsel_y_pred_int)*100))    
-
+      
     
+    # Plot results
+    # Linear Regression    
     
